fc3532395eb54c0bec73f16e5cfe25698df8883c
mayonezny <borischubuhchiev@mail.ru>
2026-03-22 15:26:47 +0300

    again

--- apps/game/vite/config.dev.ts
+++ apps/game/vite/config.dev.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/game/',
+    base: '/play/',
     plugins: [react()],
     server: {
         port: 5000,
