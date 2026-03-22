99a6f5f826ad6733c26fc502901199e426983b4b
mayonezny <borischubuhchiev@mail.ru>
2026-03-22 12:14:43 +0300

    удалены моки

--- apps/admin-panel/src/mocks/handlers/index.ts
+++
@@ -1,5 +0,0 @@
-import { adminsHandlers } from './admins';
-import { authHandlers } from './auth';
-import { lobbyHandlers } from './lobby';
-
-export const handlers = [...authHandlers, ...lobbyHandlers, ...adminsHandlers];
